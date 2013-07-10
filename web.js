5795afe1c8ffe3d28800ba8e0cafc848c0a6f8f1
Hanxiong Shi <hanxiong.shi@gmail.com>
2013-07-10 20:28:49 +0000

    test

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ app.get('/', function(request, response) {
   
   fs.readFileSync('index.html', function(err,data){
     if(err) throw err;
+    console.log(data);
     var buffer = new Buffer(data, "utf8");
     response.send(buffer.toString());
     console.log('index read to buffer');
