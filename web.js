2f6fc860c7a48832b1ff893f15b6f28f8154c6fb
Hanxiong Shi <hanxiong.shi@gmail.com>
2013-07-10 19:57:34 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buffer = new Buffer(16);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2! ');
+  
+  fs.writeFile(buffer, index.html, function(err){
+    if(err) throw err;
+    console.log('index read to buffer');
+  });
+  response.send(buffer.write("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
