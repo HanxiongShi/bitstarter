c0f8aca303f78c220f40d1b20cd3574c1a54fc2d
Hanxiong Shi <hanxiong.shi@gmail.com>
2013-07-11 02:24:06 +0000

    test

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 app.get('/', function(request, response) {
   var string = fs.readFileSync('index.html');
   var buffer = new Buffer(string.toString());
-  response.send(buffer.toString);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
